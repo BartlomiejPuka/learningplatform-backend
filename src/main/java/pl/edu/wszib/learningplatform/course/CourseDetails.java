dc28ae32416b66553cafc3b3a114daf91df78674
BartlomiejPuka <bartlomiej.puka@gmail.com>
2021-08-10 18:46:29 +0200

    changed sql dialect

--- src/main/java/pl/edu/wszib/learningplatform/course/CourseDetails.java
+++ src/main/java/pl/edu/wszib/learningplatform/course/CourseDetails.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 public class CourseDetails {
     private String author;
-    @Lob
+    @Column(columnDefinition="TEXT")
     private String description;
 
     private String iconUrl;
